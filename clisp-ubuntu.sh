544c838187f0d179f84f721fe01dadf6bcb3bdd8
Zlatan Vasović <zlatanvasovic@gmail.com>
2012-12-28 05:22:51 -0800

    Create clisp-ubuntu.sh

---
+++ clisp-ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script - CLISP installation
+# Written by Zlatan Vasović (ZDroid) @ 2012
+
+sudo apt-get install clisp
+
+# For running uncomment (remove #) from line down
+# clisp
+# For installing Emacs and SLIME uncomment (remove #) from line down
+# sudo apt-get install emacs slime
+# For conf see README
