0aaf87445edc6054a167033f50a6672f1ec4c437
Zlatan Vasović <zlatanvasovic@gmail.com>
2012-12-29 19:27:16 +0100

    Small fix

--- clisp-ubuntu.sh
+++ clisp-ubuntu.sh
@@ -6,6 +6,7 @@ sudo apt-get install clisp
 
 # For running uncomment (remove #) from line down
 # clisp
+
 # For installing Emacs and SLIME uncomment (remove #) from line down
 # sudo apt-get install emacs slime
-# For Emacs conf see README
+# For Emacs conf see README.md
