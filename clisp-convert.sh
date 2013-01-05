ae123ca1d779bd79c2c0ae799bc67da2cae740e2
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-01-06 00:42:17 +0100

    Added new note (+x file permissions)

--- clisp-convert.sh
+++ clisp-convert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script - CLISP conversion from C° to °F
 # Written by Zlatan Vasović (ZDroid) @ 2012
+# You need to set +x (executable) permissions for this file
 clear
 echo "This is script - CLISP conversion from C° to °F"
 echo "For example is taken 10° C"
