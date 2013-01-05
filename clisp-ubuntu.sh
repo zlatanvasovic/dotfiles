3488f03eeb9d7e75d02683e8e2b875708b7a7c83
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-01-06 00:33:30 +0100

    Added new comment

--- clisp-ubuntu.sh
+++ clisp-ubuntu.sh
@@ -10,3 +10,4 @@ sudo apt-get install clisp
 # For installing Emacs and SLIME uncomment (remove #) from line down
 # sudo apt-get install emacs slime
 # For Emacs conf see README.md
+# You can also download .emacs file (CLISP conf) from this GitHub repo
