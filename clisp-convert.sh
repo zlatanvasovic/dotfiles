7973513e9f7fdefbdc846efd838dd69102c78c8f
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-01-11 19:51:44 +0100

    Added note

--- clisp-convert.sh
+++ clisp-convert.sh
@@ -2,6 +2,7 @@
 # Script - CLISP conversion from C° to °F
 # Written by Zlatan Vasović (ZDroid) @ 2012
 # You need to set +x (executable) permissions for this file
+# WARN: This is tetsing script; may not work
 clear
 echo "This is script - CLISP conversion from C° to °F"
 echo "For example is taken 10° C"
